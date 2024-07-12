7c4c918c86be5dceebee21b39e068b021f37ebc0
srisailavanya <srisailavanyamukiri@gmail.com>
2024-07-12 12:00:33 +0530

    second commit

---
+++ pattren.java
@@ -0,0 +1,14 @@
+class pattern
+
+public static void main(string args[])
+{
+  char i,j,n;
+   for(i=1; i<=n;i++)
+    {
+       for(j=1;j<=i;j++)
+         {
+           System.out.println("*");
+            }
+           System.out.println(" ");
+         }
+}
